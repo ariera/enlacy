6db78a4160242afadabddc5a1ee08e43bf81db91
ariera <ariera@gmail.com>
2011-04-30 17:59:27 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Enlacy::Application.config.secret_token = '4f7e7210804e7879a0f1298317240451f2b5f0a0c55d7ce659319bdc980571504731a52951f214078beeaa256c6205b65c565cbab439bf605199cc83979a202f'
