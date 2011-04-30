10800611c0f9881eee8c7eb350023d17109bf361
ariera <ariera@gmail.com>
2011-04-30 21:00:33 +0200

    first views working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Enlacy::Application.routes.draw do
+  get "home/index"
+
   devise_for :users
+  resources :home, :only => :index
+  root :to => 'home#index'
+  
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
