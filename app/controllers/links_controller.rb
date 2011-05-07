95e39fe8a8cb7a77e80be9689c4ea76fd7a782fe
ariera <ariera@gmail.com>
2011-05-07 17:16:20 +0200

    crear y borrar links

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -3,4 +3,15 @@ class LinksController < ApplicationController
     user=User.find_by_login(params[:login]) || User.first
     @links= user.links
   end
+  
+  def create
+    link = Link.new(params[:link])
+    link.title = link.url
+    current_user.links << link
+    redirect_to home_path(current_user)
+  end
+  
+  def destroy
+    render :text => params[:id]
+  end
 end
